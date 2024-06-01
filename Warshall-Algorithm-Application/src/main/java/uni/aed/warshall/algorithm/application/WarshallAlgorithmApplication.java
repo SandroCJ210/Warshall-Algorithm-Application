abd21016c24238f8a0dec93ce347b911f399b162
SandroCJ210 <sandro.carrillo.j@uni.pe>
2024-05-31 22:35:39 -0500

    first commit

---
+++ Warshall-Algorithm-Application/src/main/java/uni/aed/warshall/algorithm/application/WarshallAlgorithmApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uni.aed.warshall.algorithm.application;
+
+/**
+ *
+ * @author sandro
+ */
+public class WarshallAlgorithmApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
