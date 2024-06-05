1b0bf316d434c9444ec8748278b6442b0bd7b729
Alkerg <albert.arg500@gmail.com>
2024-06-05 03:17:57 -0500

    Algorithm added, first commit

---
+++ Warshall-Algorithm-Application/src/main/java/uni/aed/warshall/algorithm/application/FWAlgorithm.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uni.aed.warshall.algorithm.application;
+
+/**
+ *
+ * @author Albert
+ */
+public class FWAlgorithm {
+    final static int INF = 9999999;
+    public int V;
+    public int[][] routes;
+    public int[][] dist;
+
+    public FWAlgorithm(int nodes){
+        V = nodes;
+    }
+    public void Solve(int graph[][]) {
+        dist = new int[V][V];
+        routes = new int[V][V];
+        int i, j, k;
+
+        // Inicializar la matriz de distancias
+        for (i = 0; i < V; i++)
+            for (j = 0; j < V; j++)
+                dist[i][j] = graph[i][j];
+
+        // Actualizar la matriz de distancias
+        for (k = 0; k < V; k++) {
+            for (i = 0; i < V; i++) {
+                for (j = 0; j < V; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j]){
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        routes[i][j] = k;
+                    }
+                }
+            }
+        }
+
+        // Imprimir la matriz de distancias
+        PrintSolution(dist);
+    }
+
+    public void PrintSolution(int dist[][]) {
+        System.out.println("La matriz de distancias más cortas entre cada par de vértices es:");
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] == INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j] + "   ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public void PrintNodes(int graph[][]){
+        for(int i=0; i < V; i++){
+            for(int j=0; j < V; j++){
+                if(graph[i][j] != INF && graph[i][j] != 0)
+                    System.out.println("Node " + i + " -> Node " + j);   
+            }
+        }
+    }
+    
+    public void PrintRoutesMatrix(){
+        System.out.println("Matriz de rutas:");
+        for(int i=0; i < V; i++){
+            for(int j=0; j < V; j++)
+                System.out.print(routes[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
--- Warshall-Algorithm-Application/src/main/java/uni/aed/warshall/algorithm/application/WarshallAlgorithmApplication.java
+++ Warshall-Algorithm-Application/src/main/java/uni/aed/warshall/algorithm/application/WarshallAlgorithmApplication.java
@@ -3,6 +3,7 @@
  */
 
 package uni.aed.warshall.algorithm.application;
+import static uni.aed.warshall.algorithm.application.FWAlgorithm.INF;
 
 /**
  *
@@ -11,6 +12,26 @@ package uni.aed.warshall.algorithm.application;
 public class WarshallAlgorithmApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FWAlgorithm FWA = new FWAlgorithm(5);
+        
+        int graph1[][] = { {0, 5, INF, 10},
+                          {INF, 0, 3, INF},
+                          {INF, INF, 0, 1},
+                          {INF, INF, INF, 0}
+                        };
+        int graph2[][] = { {0, 3, INF, 7},
+                          {8, 0, 2, INF},
+                          {5, INF, 0, 1},
+                          {2, INF, INF, 0}
+                        };
+        int graph3[][] = { {0, 2, INF, -4, INF},
+                          {INF, 0, -2, 1, 3},
+                          {INF, INF, 0, INF, 1},
+                          {INF, INF, INF, 0, 4},
+                          {INF, INF, INF, INF, 0}
+                        };
+        FWA.PrintNodes(graph3);
+        FWA.Solve(graph3);
+        FWA.PrintRoutesMatrix();
     }
 }
